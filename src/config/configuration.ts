5c0b1a6ea9db1ea9dfbf5f3489687bd265a29e41
王瑞鑫 <ruixin.wang@hand-china.com>
2023-12-27 20:22:57 +0800

    fix config

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -5,9 +5,9 @@ export default () => ({
     port: parseInt(process.env.DATABASE_PORT, 10) || 3306,
   },
   kook_bot: {
-    token: '1/MjM2MjI=/Xc5ppF2dw16c1fUqvWL78A==',
-    verify_token: 'cG8BpdpCtKhE9wbf',
-    encrypt_key: 'yyRka7mor86',
+    token: '',
+    verify_token: '',
+    encrypt_key: '',
     version: '0.0.1',
     api: {
       host: 'https://www.kookapp.cn/api/v3',
@@ -21,8 +21,8 @@ export default () => ({
   },
   baidu: {
     token_api: 'https://aip.baidubce.com/oauth/2.0/token',
-    ak: 'f0Gu1iChx4G2bFCpACt9gQOj',
-    sk: 'oCLjswylSvCmyAYIigSzQxEwHByqbMTv',
+    ak: '',
+    sk: '',
     chat_api:
       'https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/eb-instant',
   },
